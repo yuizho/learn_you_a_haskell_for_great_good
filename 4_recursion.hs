8961e102a748872f494b529d84c1b448ee58aa2f
yuizho <yuizho3@gmail.com>
2026-03-30 23:24:05 +0900

    add section 4 codes

---
+++ 4_recursion.hs
@@ -0,0 +1,30 @@
+-- 4.1
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "empty list"
+maximum' [x] = x
+maximum' (x : xs) = max x (maximum' xs)
+
+take' :: Int -> [a] -> [a]
+take' n _
+  | n <= 0 = []
+take' _ [] = []
+take' n (x : xs) = x : take' (n - 1) xs
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x : xs) (y : ys) = (x, y) : zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x : xs)
+  | a == x = True
+  | otherwise = a `elem'` xs
+
+-- 4.3 quick sort
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x : xs) =
+  let small = [a | a <- xs, a <= x]
+      large = [a | a <- xs, a > x]
+   in quicksort small ++ [x] ++ large
