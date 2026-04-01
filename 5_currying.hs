8e25cf4fb8b3a1131d852722bf9eec1b5c26a425
yuizho <yuizho3@gmail.com>
2026-04-01 23:47:35 +0900

    add section 5 codes

--- 5_currying.hs
+++ 5_currying.hs
@@ -12,3 +12,18 @@ largestDivisible :: Integer
 largestDivisible = head (filter p [100000, 99999 ..])
   where
     p x = x `mod` 3829 == 0
+
+-- 5.5.1 左畳み込み
+-- ポイントフリースタイルによる左畳み込みでのsum実装
+sum' :: (Num a) => [a] -> a
+sum' = foldl (+) 0
+
+-- 5.5.2 右畳み込み
+-- 左畳み込みだとリストから新しいリストを構築するときに ++ 使わないといけなくと遅いので、このケースは右畳み込みを使うことが多い
+map' :: (a -> b) -> [a] -> [b]
+map' f xs = foldr (\x acc -> f x : acc) [] xs
+
+-- 5.5.6 無限リストを畳み込む
+-- fldrで実装しているので無限リストも処理可能 (Falseがあったら打ち切る)
+and' :: [Bool] -> Bool
+and' xs = foldr (&&) True xs
