6913a582231155ecf117b84fe806186e88b70ba9
yuizho <yuizho3@gmail.com>
2026-04-04 19:57:50 +0900

    add section 7 codes

--- 7_type.hs
+++ 7_type.hs
@@ -1,6 +1,18 @@
-data Shape = Circle Float Float Float | Rectangle Float Float Float Float
+data Point = Point Float Float deriving (Show)
+
+data Shape = Circle Point Float | Rectangle Point Point
   deriving (Show)
 
 area :: Shape -> Float
-area (Circle _ _ r) = pi * r ^ 2
-area (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
+area (Circle _ r) = pi * r ^ 2
+area (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
+
+-- 7.3 レコード構文
+data Person = Person
+  { firstName :: String,
+    lastName :: String,
+    age :: Int,
+    height :: Float,
+    phoneNumber :: String
+  }
+  deriving (Show)
