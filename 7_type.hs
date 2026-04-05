148c717256d3eebb709a9f0a1c155a60ff62ad59
yuizho <yuizho3@gmail.com>
2026-04-05 16:41:55 +0900

    add section 7 codes

--- 7_type.hs
+++ 7_type.hs
@@ -8,11 +8,46 @@ area (Circle _ r) = pi * r ^ 2
 area (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
 
 -- 7.3 レコード構文
+type PhoneNumber = String
+
 data Person = Person
   { firstName :: String,
     lastName :: String,
     age :: Int,
     height :: Float,
-    phoneNumber :: String
+    phoneNumber :: PhoneNumber
   }
-  deriving (Show)
+  deriving (Show, Eq)
+
+-- 7.7 再帰的なデータ構造
+data List a = Empty | Cons {listhead :: a, listTail :: List a}
+  deriving (Show, Read, Eq, Ord)
+
+-- 7.7.2 二分探索木
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+  | x == a = Node x left right
+  | x < a = Node a (treeInsert x left) right
+  | x > a = Node a left (treeInsert x right)
+
+-- 7.8.1. Eq型クラスの内部
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where
+  Red == Red = True
+  Green == Green = True
+  Yellow == Yellow = True
+  _ == _ = False
+
+data MyMaybe a = MyNothing | MyJust a
+
+instance (Eq m) => Eq (MyMaybe m) where
+  MyJust x == MyJust y = x == y
+  MyNothing == MyNothing = True
+  _ == _ = False
