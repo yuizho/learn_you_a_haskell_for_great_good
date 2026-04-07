ab3972cd67c5efcb3eb2daa998365bb156aa7de3
yuizho <yuizho3@gmail.com>
2026-04-08 00:09:13 +0900

    add section 9 codes

---
+++ bytestringcopy.hs
@@ -0,0 +1,23 @@
+import Control.Exception
+import qualified Data.ByteString.Lazy as B
+import System.Directory
+import System.Environment
+import System.IO
+
+main = do
+  (fileName1 : fileName2 : _) <- getArgs
+  copy fileName1 fileName2
+
+copy source dest = do
+  contents <- B.readFile source
+  bracketOnError
+    (openTempFile "." "temp")
+    ( \(tempName, tempHandle) -> do
+        hClose tempHandle
+        removeFile tempName
+    )
+    ( \(tempName, tempHandle) -> do
+        B.hPutStr tempHandle contents
+        hClose tempHandle
+        renameFile tempName dest
+    )
