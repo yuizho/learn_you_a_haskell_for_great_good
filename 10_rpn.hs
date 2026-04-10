b06c0a971bf4457b1316feaaa73c12bb38708a3c
yuizho <yuizho3@gmail.com>
2026-04-10 23:57:19 +0900

    add section 10 codes

--- 10_rpn.hs
+++ 10_rpn.hs
@@ -5,3 +5,7 @@ solveRPN = head . foldl foldingFunction [] . words
     foldingFunction (x : y : ys) "+" = (y + x) : ys
     foldingFunction (x : y : ys) "-" = (y - x) : ys
     foldingFunction xs numberString = read numberString : xs -- 最終的な戻り値の型がDoubleなので型推論できる
+
+data Section = Section {getA :: Int, getB :: Int, getC :: Int} deriving (Show)
+
+type ReadSystem = [Section]
