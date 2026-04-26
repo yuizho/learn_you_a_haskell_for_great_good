33569f8e5349f3b7bc7b190b02ae90e935e0ec5b
yuizho <yuizho3@gmail.com>
2026-04-26 22:49:32 +0900

    add section 13 codes

--- 13_monad.hs
+++ 13_monad.hs
@@ -1,3 +1,5 @@
+import Control.Monad
+
 -- 13.2
 applyMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b
 applyMaybe Nothing f = Nothing
@@ -51,3 +53,42 @@ justStr s = do
   -- 空文字とか来るとNothingがかえる
   (x : xs) <- Just s
   return x
+
+-- 13.6
+listOfTuples :: [(Int, Char)]
+listOfTuples = do
+  n <- [1, 2]
+  ch <- ['a', 'b']
+  return (n, ch)
+
+sevensOnly :: [Int]
+sevensOnly = do
+  x <- [1 .. 50]
+  guard ('7' `elem` show x)
+  return x
+
+type KnightPos = (Int, Int)
+
+moveKnight :: KnightPos -> [KnightPos]
+moveKnight (c, r) = do
+  (c', r') <-
+    [ (c + 2, r - 1),
+      (c + 2, r + 1),
+      (c - 2, r - 1),
+      (c - 2, r + 1),
+      (c + 1, r - 2),
+      (c + 1, r + 2),
+      (c - 1, r - 2),
+      (c - 1, r + 2)
+    ]
+  guard (c' `elem` [1 .. 8] && r' `elem` [1 .. 8])
+  return (c', r')
+
+in3 :: KnightPos -> [KnightPos]
+in3 start = do
+  first <- moveKnight start
+  second <- moveKnight first
+  moveKnight second
+
+canReachIn3 :: KnightPos -> KnightPos -> Bool
+canReachIn3 start end = end `elem` in3 start
