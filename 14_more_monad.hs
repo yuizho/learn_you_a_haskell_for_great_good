219ad76b1be41f1adfbd1a4da107c90e9e31bb08
yuizho <yuizho3@gmail.com>
2026-04-29 23:52:52 +0900

    add section 14 codes

--- 14_more_monad.hs
+++ 14_more_monad.hs
@@ -1,5 +1,18 @@
+import Control.Monad.Writer
+
 isBigGang :: Int -> (Bool, String)
 isBigGang x = (x > 9, "Compared gang size to 9.")
 
 applyLog :: (Monoid m) => (a, m) -> (a -> (b, m)) -> (b, m)
 applyLog (x, log) f = let (y, newLog) = f x in (y, log `mappend` newLog)
+
+-- 14.1.3
+logNumber :: Int -> Writer [String] Int
+logNumber x = writer (x, ["Got number: " ++ show x])
+
+multWithLog :: Writer [String] Int
+multWithLog = do
+  a <- logNumber 3
+  b <- logNumber 5
+  tell ["Gonna multiply there two"]
+  return (a * b)
